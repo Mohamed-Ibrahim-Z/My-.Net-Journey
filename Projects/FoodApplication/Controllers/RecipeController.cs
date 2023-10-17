d0a30b886545d86ad61c06471131f933c5302fa5
Mohamed-Ibrahim-Z <Mohammed20191700490@cis.asu.edu.eg>
2023-10-17 09:03:59 +0300

    [Started] Food Apllication

---
+++ Projects/FoodApplication/Controllers/RecipeController.cs
@@ -0,0 +1,17 @@
+﻿using FoodApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApplication.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetRecipeCard([FromBody] List<Recipe> recipes)
+        {
+            return PartialView("_RecipeCard", recipes);
+        }
+    }
+}
